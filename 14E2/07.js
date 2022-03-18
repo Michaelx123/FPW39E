b25d71929761583f7f1c300a9105e4d7a24e4e2d
Michael <mklink@yandex.ru>
2022-03-19 01:44:34 +0300

    ЗАДАНИЕ 2.7.3 (HW-03)

--- 14E2/07.js
+++ 14E2/07.js
@@ -1,4 +1,4 @@
-﻿const arr = [1, 2, null, NaN, 0, 0, 6, 7, 8, 12, undefined, 1.2, Infinity, -Infinity,'строка'];
+﻿const arr = [1, 2, null, NaN, 0, 0, 6, 7, 8, 12, undefined, 1.2, Infinity, -Infinity,'строка','0',-1.2,-2];
 
 let isEven = 0;
 let isOdd = 0;
@@ -8,7 +8,8 @@ let isOver = 0;
 arr.forEach(function(item, index, array) {
     //console.log(typeof item + ' '+isFinite(item)+' ' +(item ^ 0));
     //На четность проверяю только целые числа, вещественные отношу к иным элементам
-    if (typeof (item) !== "number" || !isFinite(item) || (item ^ 0) !== item){
+    //if (typeof (item) !== "number" || !isFinite(item) || (item ^ 0) !== item){ //Не вижу необходимости проверки на typeof (item) !== "number"
+    if (!isFinite(item) || (item ^ 0) !== item){
         isOver++;
     } else if (item == 0) {
         isZero++;        
